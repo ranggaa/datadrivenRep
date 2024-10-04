d707948f605394da3c4198c4b3d6a4e28cc94010
lalatendu11 <lalatendung@gmail.com>
2024-10-04 19:32:49 +0530

    New Class Created

---
+++ src/commonFunctions/Demo.java
@@ -0,0 +1,22 @@
+package commonFunctions;
+
+public class Demo {
+public void add()
+{
+	int a=23,b=78,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=45,b=7,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void sub()
+{
+	int a=54,b=2,c;
+	c=a+b;
+	System.out.println(c);
+}
+}
